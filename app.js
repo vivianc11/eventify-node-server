513d67a11b6b6c05e551bb43392087235e94c311
Vivian Chen <vivian.chen1193@gmail.com>
2023-06-21 17:17:12 -0700

    established connection to database

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const PORT = 3000;
+
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("Our DB is connected!!")
+    })
+    .catch(error => console.log(error.message));
 
 app.get('/', (req, res) => {
     res.send('')

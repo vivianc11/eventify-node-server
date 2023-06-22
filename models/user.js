4aaaf4a869287ecb426eda63a3da4bb624df50e7
Vivian Chen <vivian.chen1193@gmail.com>
2023-06-21 17:50:28 -0700

    created user schema

---
+++ models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+model.exports = mongoose.model('User', userSchema);
